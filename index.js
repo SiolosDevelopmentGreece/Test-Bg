f7fde95d32e37cd9d32210212d4d75d08bf7379c
SiolosDevelopmentGreece <148241993+SiolosDevelopmentGreece@users.noreply.github.com>
2025-02-13 14:04:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Playing Test Bot2", "Playing Test Bot2"];
+const statusMessages = ["Playing Baily Service", "Playing Baily Service"];
 let currentIndex = 0;
 
 async function login() {
